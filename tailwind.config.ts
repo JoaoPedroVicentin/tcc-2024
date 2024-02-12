0bfb0b4efba76039239a809ac36cf93b23f39958
JOAO PEDRO LOPES VICENTIN <jplvicen@gmail.com>
2024-02-12 20:32:37 -0300

    feat: configuracoes iniciais para o eslint e tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,35 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        inter: ['"Inter"', 'sans-serif'],
+      },
+      colors: {
+        theme: {
+          white: '#ECECEB',
+          black: '#020202',
+          brown: '#8A733F',
+          gray: {
+            50: '#A6A6A6',
+            100: '#5E5D59',
+          },
+          green: {
+            50: '#DCEAA8',
+            100: '#C5EEAE',
+            200: '#D5FF29',
+          },
+        },
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
