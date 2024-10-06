fe2f5be42598f1c8fc9416f45894b1bbf91b8007
JOAO PEDRO LOPES VICENTIN <jplvicen@gmail.com>
2024-10-06 00:23:14 -0300

    ajusta responsividade

--- src/components/wrapperList/index.tsx
+++ src/components/wrapperList/index.tsx
@@ -2,7 +2,7 @@ import { IWrapperListProps } from './interface/wrapperListProps.interface'
 
 export function WrapperList({ children }: IWrapperListProps) {
   return (
-    <main className="h-full p-section">
+    <main className="h-full p-sectionMd lg:p-section">
       <div className="mx-auto flex max-w-screen-2xl flex-col gap-9">
         {children}
       </div>
